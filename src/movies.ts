c3f47223fcc98d520a1d2b7ed072a75cf71e06a8
Christian <chrisdaq@gmail.com>
2019-08-11 15:44:13 -0400

    add 'post' route for creating movies,  the other ones are similar 'delete', 'put'

--- src/movies.ts
+++ src/movies.ts
@@ -3,8 +3,20 @@ const router = Router();
 
 const movies = require('./sample.json');
 
-router.get('/', (req, res) => {
+router.get('/', (req:any, res:any) => {
 	res.json(movies);
+});
+
+router.post('/', (req:any, res:any) => {
+	const { title, director, year, rating } = req.body;
+	if(title && director && year && rating) {
+		const id = movies.length + 1;// simulating id generation
+		const newMovie = { ...req.body, id };
+		movies.push(newMovie);
+		res.json(movies);
+	} else {
+		res.status(500).send('wrong request');
+	}
 });
 
 module.exports = router;
