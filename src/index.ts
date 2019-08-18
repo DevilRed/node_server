604cfa89ad3f9e814bee507bfbf22b6e8d44c16c
Christian <chrisdaq@gmail.com>
2019-08-18 15:08:17 -0400

    installed required packages and set comments for them in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-// https://www.youtube.com/watch?v=bK3AJfs7qNY
+// https://www.youtube.com/watch?v=-bI0diefasA
+// app code https://www.youtube.com/redirect?redir_token=e8IBCYgkieyhO91bxy6LdTMO8Rx8MTU2NjIzMjgyNUAxNTY2MTQ2NDI1&event=video_description&v=-bI0diefasA&q=https%3A%2F%2Fgithub.com%2FFaztTech%2Fnodejs-notes-app
 /*
 npm init - y
 npm i - D express morgan nodemon
 		(morgan is a middleware to allow to see request on the console, nodemon restat the server after a change)
+
+additional modules:
+* express-handlebars, is a template engine
+* express-session, to create session in the server
+* method-override, to extend html form functionality, GET POST PUT DELETE
+* mongoose, ORM for mongoDB
+* passport passport-local, for login
+* bcryptjs, hash password
+* connect-flash, for flash messages
+
+from 7.30
 */
 const express = require('express');
 const app = express();
